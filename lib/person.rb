79da999e3d1e1d7e8be9be34eb41dc686d9b200c
Brandon Teich <brandonteich123@gmail.com>
2018-11-13 23:54:38 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name=(personname)
+    @name = personname
+  end 
+  def name 
+    @name 
+  end 
+  def job=(jobtype)
+    @job = jobtype
+  end 
+  def job 
+    @job 
+  end 
+end  
